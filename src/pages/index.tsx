9602c59645ab75d99d7dab6ea04981e775de005d
Thiago <thiagooffice2@gmail.com>
2022-01-06 23:51:54 -0300

    Start project

---
+++ src/pages/index.tsx
@@ -0,0 +1,26 @@
+import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from '../core/Cliente'
+
+
+export default function Home() {
+
+  const clientes = [
+    new Cliente("Ana", 34, "1"),
+    new Cliente("Bia", 45, "2"),
+    new Cliente("Carlos", 23, "3"),
+    new Cliente("Pedro", 54, "4"),
+  ]
+
+  return (
+    <div className={`
+    flex items-center justify-center h-screen 
+    bg-gradient-to-r from-blue-500 to-purple-500
+    text-white
+    `}>
+     <Layout titulo="Cadastro Simples">
+       <Tabela clientes={clientes}></Tabela>
+     </Layout>
+    </div>
+  )
+}
